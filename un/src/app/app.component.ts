e53300ac53851c78c575f2afc937063755ee1365
develop <ikhlas.firlana@gmail.com>
2019-02-15 10:52:36 +0700

    no message

---
+++ un/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'un-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'un';
+}
